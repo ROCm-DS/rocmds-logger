f6a0884834c2c2f2cb66120dd708707eaa0ebfe5
domcharrier <docharri@amd.com>
2025-05-05 13:47:06 +0000

    rapids_config.cmake: use ROCmDS-CMake,configurable
    - By default fetch from ROCm-DS/ROCmDS-CMake release 2.0.x.
    - Allow to configure from which URL/branch to obtain RAPIDS.cmake script from
      and to use ROCmDS-CMake modules via CMAKE_MODULE_PATH instead.
    
    Co-authored-by: gpinkert <gpinkert@amd.com>

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -15,8 +15,31 @@
 # I expect it to be relatively static and not something we need to re-release
 # often. Furthermore, we won't be building packages of it since we only ever
 # need it cloned by CPM during the builds of other packages.
+# =============================================================================
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# =============================================================================
 file(READ "${CMAKE_CURRENT_LIST_DIR}/VERSION" _rapids_version)
-if(_rapids_version MATCHES [[^([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])]])
+if(_rapids_version MATCHES [[^([0-9][0-9]?)\.([0-9][0-9]?)\.([0-9][0-9])]])
   set(RAPIDS_VERSION_MAJOR "${CMAKE_MATCH_1}")
   set(RAPIDS_VERSION_MINOR "${CMAKE_MATCH_2}")
   set(RAPIDS_VERSION_PATCH "${CMAKE_MATCH_3}")
@@ -29,10 +52,48 @@ else()
       "Could not determine RAPIDS version. Contents of VERSION file:\n${_rapids_version_formatted}")
 endif()
 
-if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
-  file(
-    DOWNLOAD
-    "https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_VERSION_MAJOR_MINOR}/RAPIDS.cmake"
-    "${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
+set(
+  RAPIDS_CMAKE_MODULE_PATH
+  $ENV{RAPIDS_CMAKE_MODULE_PATH}
+  CACHE FILEPATH
+  "Announce that ROCmDS-CMake is available via the provided module path."
+)
+if (NOT "${RAPIDS_CMAKE_MODULE_PATH}" STREQUAL "")
+  list(APPEND CMAKE_MODULE_PATH "${RAPIDS_CMAKE_MODULE_PATH}")
+  # NOTE(HIP/AMD): needed to set rapids-cmake-dir variable
+  include(rapids-cmake)
+  return()
+endif()
+if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake)
+  if(DEFINED ENV{RAPIDS_CMAKE_SCRIPT_REPO})
+    set(RAPIDS_CMAKE_SCRIPT_REPO "$ENV{RAPIDS_CMAKE_SCRIPT_REPO}")
+  else()
+    set(RAPIDS_CMAKE_SCRIPT_REPO ROCm-DS/ROCmDS-CMake)
+  endif()
+  if(DEFINED ENV{RAPIDS_CMAKE_SCRIPT_BRANCH})
+    set(RAPIDS_CMAKE_SCRIPT_BRANCH "$ENV{RAPIDS_CMAKE_SCRIPT_BRANCH}")
+  else()
+    set(RAPIDS_CMAKE_SCRIPT_BRANCH release/2.0.x)
+  endif()
+
+  set(URL "https://raw.githubusercontent.com/${RAPIDS_CMAKE_SCRIPT_REPO}/${RAPIDS_CMAKE_SCRIPT_BRANCH}/RAPIDS.cmake")
+  file(DOWNLOAD ${URL}
+    ${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake
+    STATUS DOWNLOAD_STATUS
+  )
+  list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
+  list(GET DOWNLOAD_STATUS 1 ERROR_MESSAGE)
+
+  if(${STATUS_CODE} EQUAL 0)
+    message(STATUS "Downloaded 'RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake' successfully!")
+  else()
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake)
+    # for debuging: message(FATAL_ERROR "Failed to download 'RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake'. URL: ${URL}, Reason: ${ERROR_MESSAGE}")
+    message(FATAL_ERROR "Failed to download 'RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake'. Reason: ${ERROR_MESSAGE}")
+  endif()
+endif()
+
+if(DEFINED ENV{RAPIDS_CMAKE_BRANCH})
+  set(rapids-cmake-branch $ENV{RAPIDS_CMAKE_BRANCH})
 endif()
 include("${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
