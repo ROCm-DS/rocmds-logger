1a1088cb408e8a8f8ba10c8bef18fbd05f1ddf0d
Kevin Joseph <kevin.joseph@amd.com>
2025-11-24 19:59:44 +0000

    (fix) Add additional search path when trying to load librapids_logger.so
    CMake through the GNUInstallDirs module can sometimes install
    libraries to <PREFIX>/lib as opposed to <PREFIX>/lib64 on some Linux
    distributions. This change is to add an additional library search path
    in load.py incase we fail to find a .so in the lib64 directory.

--- python/rapids-logger/rapids_logger/load.py
+++ python/rapids-logger/rapids_logger/load.py
@@ -35,6 +35,10 @@ def _load_wheel_installation(soname: str):
         lib := os.path.join(os.path.dirname(__file__), "lib64", soname)
     ):
         return ctypes.CDLL(lib, PREFERRED_LOAD_FLAG)
+    if os.path.isfile(
+        lib := os.path.join(os.path.dirname(__file__), "lib", soname)
+    ):
+        return ctypes.CDLL(lib, PREFERRED_LOAD_FLAG)
     return None
 
 
